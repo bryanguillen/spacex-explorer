d14f68ca54acbbfae6eb5cbc86edb6a979aff139
bryg217 <bryan.guillen217@gmail.com>
2021-10-28 17:40:50 -0400

    feat(nav): add navigation side initial implementation

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -4,42 +4,13 @@ import { MdHome, MdHistory, MdUpcoming } from 'react-icons/md';
 
 import './Navigation.css';
 
+const iconsStyles = { color: '#C9D1D9', size: '2rem' };
+
 interface NavigationProps {
   historyButtonText: string
   homeButtonText: string
   upcomingButtonText: string
 }
-
-const NavigationFooter = ({
-  historyButtonText,
-  homeButtonText,
-  upcomingButtonText
-}: NavigationProps) => {
-  return (
-    <IconContext.Provider value={{ color: '#C9D1D9', size: '2rem' }}>
-      <div className="navigation-footer">
-        <div className="navigation-footer-button navigation-footer-button-home" role="button">
-          <MdHome/>
-          <NavigationFooterText
-            buttonText={homeButtonText}
-          />
-        </div>
-        <div className="navigation-footer-button navigation-footer-button-create" role="button">
-          <MdHistory/>
-          <NavigationFooterText
-            buttonText={historyButtonText}
-          />
-        </div>
-        <div className="navigation-footer-button navigation-footer-button-search" role="button">
-          <MdUpcoming/>
-          <NavigationFooterText
-            buttonText={upcomingButtonText}
-          />
-        </div>
-      </div>
-    </IconContext.Provider>
-  );
-};
 
 interface NavigationTextProps {
   buttonText: string
@@ -51,8 +22,79 @@ const NavigationFooterText = ({
   return (
     <span className="navigation-footer-text">{buttonText}</span>
   );
-}
+};
+
+const NavigationFooter = ({
+  historyButtonText,
+  homeButtonText,
+  upcomingButtonText
+}: NavigationProps) => {
+  return (
+    <IconContext.Provider value={iconsStyles}>
+      <div className="navigation-footer">
+        <div className="navigation-footer-button" role="button">
+          <MdHome/>
+          <NavigationFooterText
+            buttonText={homeButtonText}
+          />
+        </div>
+        <div className="navigation-footer-button" role="button">
+          <MdHistory/>
+          <NavigationFooterText
+            buttonText={historyButtonText}
+          />
+        </div>
+        <div className="navigation-footer-button" role="button">
+          <MdUpcoming/>
+          <NavigationFooterText
+            buttonText={upcomingButtonText}
+          />
+        </div>
+      </div>
+    </IconContext.Provider>
+  );
+};
+
+const NavigationSideText = ({
+  buttonText
+}: NavigationTextProps) => {
+  return (
+    <span className="navigation-side-text">{buttonText}</span>
+  );
+};
+
+const NavigationSide = ({
+  historyButtonText,
+  homeButtonText,
+  upcomingButtonText
+}: NavigationProps) => {
+  return (
+    <IconContext.Provider value={iconsStyles}>
+      <div className="navigation-side">
+        <div className="navigation-side-button" role="button">
+          <MdHome/>
+          <NavigationSideText
+            buttonText={homeButtonText}
+          />
+        </div>
+        <div className="navigation-side-button" role="button">
+          <MdHistory/>
+          <NavigationSideText
+            buttonText={historyButtonText}
+          />
+        </div>
+        <div className="navigation-side-button" role="button">
+          <MdUpcoming/>
+          <NavigationSideText
+            buttonText={upcomingButtonText}
+          />
+        </div>
+      </div>
+    </IconContext.Provider>
+  );
+};
 
 export {
-  NavigationFooter
+  NavigationFooter,
+  NavigationSide
 };
