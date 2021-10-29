604ee9b411d696bc55f040e6b43e626a4c5003f1
bryg217 <bryan.guillen217@gmail.com>
2021-10-29 16:56:17 -0400

    feat: add support for truncating details.

--- components/mission-feed-card/MissionFeedCard.tsx
+++ components/mission-feed-card/MissionFeedCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppButton from '../app-button/AppButton';
 import MissionTextField, { MissionTextFieldProps } from '../mission-text-field/MissionTextField';
+import truncateDetails from './truncate-details';
 
 import './MissionFeedCard.css';
 
@@ -23,7 +24,7 @@ const MissionFeedCard = ({
     <div className={`mission-feed-card ${className ? className : ''}`}>
       <div className="mission-feed-card-field mission-feed-card-name dark-mode-font-color">{missionName}</div>
       <MissionTextField className="mission-feed-card-field" name={missionDate.name} value={missionDate.value}/>
-      <MissionTextField className="mission-feed-card-field" name={missionDetails.name} value={missionDetails.value}/>
+      <MissionTextField className="mission-feed-card-field" name={missionDetails.name} value={truncateDetails(missionDetails.value)}/>
       <div className="mission-feed-card-button-container">
         <AppButton
           text={readMoreText}
