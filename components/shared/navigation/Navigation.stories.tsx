6975374e119b7bd4b23e4741e49bdc6a8a8bc063
bryg217 <bryan.guillen217@gmail.com>
2021-10-29 19:34:51 -0400

    refactor: rename file to reflect no more footer

--- components/shared/navigation/Navigation.stories.tsx
+++ components/shared/navigation/Navigation.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavigationFooter } from './Navigation';
+import NavigationFooter from './Navigation';
 
 export default {
   title: 'Components/NavigationFooter',
