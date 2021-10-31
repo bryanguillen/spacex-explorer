39af12dd6034b48b330040be80509e2e6a25bede
bryg217 <bryan.guillen217@gmail.com>
2021-10-31 15:42:59 -0400

    feat: add AppTemplate and global content for navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import AppTemplate from '../app-template/AppTemplate';
+import content from '../content/mock-cms.json';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const {
+    homeButtonText,
+    historyButtonText,
+    upcomingButtonText
+  } = content.global;
+
+  return (
+    <AppTemplate
+      homeButtonText={homeButtonText}
+      historyButtonText={historyButtonText}
+      isHomePage={true}
+      upcomingButtonText={upcomingButtonText}
+    >
+      <Component {...pageProps} />
+    </AppTemplate>
+  )
 }
 
 export default MyApp
