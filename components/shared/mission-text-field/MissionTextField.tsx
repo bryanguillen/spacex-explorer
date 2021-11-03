b2ab9d3ce414f91bc1690e36a56157b59f78f5ff
bryg217 <bryan.guillen217@gmail.com>
2021-11-03 11:36:00 -0400

    feat: add support for hide/show to account for no content/value

--- components/shared/mission-text-field/MissionTextField.tsx
+++ components/shared/mission-text-field/MissionTextField.tsx
@@ -8,15 +8,25 @@ export interface MissionTextFieldProps {
   value: string
 }
 
+/**
+ * @description Component for a text field (i.e. FIELD_NAME: FIELD_VALUE);
+ * example: Details: This was our first...  Notice that null is returned if the
+ * value is falsy.  This is a hack used so that the component only renders when there
+ * is content (i.e. a value).  The reason for this is that it is unclear what can be
+ * returned as empty and not empty within the SpaceX API.  In other words, it seems like
+ * a field like details can be empty, whereas mission name cannot.
+ */
 const MissionTextField = ({
   name,
   value,
   className
 }: MissionTextFieldProps) => {
   return (
-    <div className={`${styles.base} dark-mode-font-color ${className ? className : ''}`}>
-      <span className={styles.name}>{name}</span>: {value}
-    </div>
+    value ? 
+      <div className={`${styles.base} dark-mode-font-color ${className ? className : ''}`}>
+        <span className={styles.name}>{name}</span>: {value}
+      </div> :
+      null
   );
 };
 
