938d9fb2c728f939e117cdb567fcc6ec7ab12669
bryg217 <bryan.guillen217@gmail.com>
2021-11-06 17:10:58 -0400

    test: add support for prod tests

--- tests-e2e/test-utils.js
+++ tests-e2e/test-utils.js
@@ -4,6 +4,6 @@ export const goBack = ClientFunction(() => window.history.back());
 
 export const getLocation = ClientFunction(() => document.location.href);
 
-export const getBaseUrl = () => process.argv.includes('prod') ? '' : 'localhost:3000';
+export const getBaseUrl = () => process.argv.includes('prod') ? 'https://zen-lumiere-da734a.netlify.app' : 'localhost:3000';
 
 export const scrollToBottom = ClientFunction(() => window.scrollTo(0,document.body.scrollHeight));
