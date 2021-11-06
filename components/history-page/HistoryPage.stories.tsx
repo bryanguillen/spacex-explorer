ccaf3571f77d255309dd594ba8067511c3e50fc1
bryg217 <bryan.guillen217@gmail.com>
2021-11-06 14:59:51 -0400

    doc: rm todo comment

--- components/history-page/HistoryPage.stories.tsx
+++ components/history-page/HistoryPage.stories.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import HistoryPage from './HistoryPage';
 
-/**
- * @TODO Add story for fetching more data
- */
-
 export default {
   title: 'Pages/HistoryPage',
   component: HistoryPage
